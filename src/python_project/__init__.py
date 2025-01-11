f6faae623dddfb21bc294579ce4fee0a36008c34
“Maëlys <jfimbett@gmail.com>
2025-01-11 22:59:28 +0100

    feat: ajout d'une nouvelle fonctionnalité

--- src/python_project/__init__.py
+++ src/python_project/__init__.py
@@ -1,3 +1,13 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("python_project")
+
+{
+  "branches": ["main", "master"],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git"
+  ]
+}
