72ded08df9355f5eb8ccb2f563fb1e01d70ed6e2
“Maëlys <jfimbett@gmail.com>
2025-01-11 22:21:55 +0100

    test-initi

--- src/python_project/python_project.py
+++ src/python_project/python_project.py
@@ -1,1 +1,12 @@
-First Committ
+##First Committ
+
+import numpy as np
+import pybacktestchain as pbc
+
+# Exemple d'utilisation de numpy
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+
+# Afficher les attributs disponibles du module
+print(dir(pbc))
+
