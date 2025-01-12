7b70aab718e04ced8cd21dc452ae52071311305f
“Maëlys <jfimbett@gmail.com>
2025-01-12 12:01:35 +0100

    Added pybacktestchain as a submodule

--- src/python_project/python_project.py
+++ src/python_project/python_project.py
@@ -1,15 +1,42 @@
 ##First Committ
+# Exemple d'utilisation de numpy
+##array = np.array([1, 2, 3, 4, 5])
+##print(array)
+# Afficher les attributs disponibles du module
+##Fprint(dir(pbc))
 
 import numpy as np
 import pybacktestchain as pbc
+import logging
+from datetime import datetime, timedelta
+import pandas as pd
+import logging
+from dataclasses import dataclass
+from datetime import datetime
+import os 
+import pickle
+from pybacktestchain.broker import Backtest  
+from pybacktestchain.utils import generate_random_name
+from pybacktestchain.data_module import UNIVERSE_SEC, get_stocks_data, DataModule, Information
 
-# Exemple d'utilisation de numpy
-array = np.array([1, 2, 3, 4, 5])
-print(array)
+# Configuration de base du logging pour afficher les logs dans la console
+logging.basicConfig(level=logging.INFO)
 
-# Afficher les attributs disponibles du module
-print(dir(pbc))
 
-def hello_world():
-    print("Hello, world!")
-    print("This is a small fix or feature!")
+
+# Définir les dates de début et de fin du backtest
+initial_date = datetime(2020, 1, 1)
+final_date = datetime(2021, 1, 1)
+
+# Créer une instance de la classe Backtest
+backtest = Backtest(
+    initial_date=initial_date,
+    final_date=final_date,
+    verbose=True  # Vous pouvez définir verbose à True pour activer les logs détaillés
+)
+
+# Lancer le backtest
+backtest.run_backtest()
+
+
+
--- tests/test_python_project.py
+++ tests/test_python_project.py
@@ -1,1 +1,6 @@
 from python_project import python_project
+# Exemple pour vérifier compute_information
+information_set = info.compute_information(t)
+logging.info(f"Information set at {t}: {information_set}")  # Vérifiez si les informations sont valides
+
+portfolio = info.compute_portfolio(t, information_set)
